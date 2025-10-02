5c5c35796f2db8f4d31a1bc39bf6c87e8fbbdbe1
cristopher.salyrosas <cristopher.salyrosas@intellicare-peru.com>
2025-10-02 01:36:58 -0500

    DockerLastTry(still dont working)

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,10 @@
 import reflex as rx
 
+# Configura Reflex para tu proyecto
 config = rx.Config(
     app_name="Proyecto_ResonancIA",
     plugins=[
-        rx.plugins.SitemapPlugin(),
-        rx.plugins.TailwindV4Plugin(),
+        rx.Sitemap(),  # Usando el plugin de Sitemap directamente
+        rx.TailwindV4(),  # Usando el plugin de Tailwind directamente
     ]
 )
